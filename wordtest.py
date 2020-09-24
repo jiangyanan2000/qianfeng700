ac6d793e83a897276b1a1988a284094f07af42d0
jiangyanan2000 <49583635@qq.com>
2020-09-24 15:23:38 +0800

    12

--- wordtest.py
+++ wordtest.py
@@ -1,5 +1,7 @@
+import docx
 from docx import Document
 from docx.shared import RGBColor, Inches,Pt,Cm
+from docx.oxml.ns import qn
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT,WD_UNDERLINE,WD_TAB_ALIGNMENT,WD_TAB_LEADER
 import os
 
@@ -12,9 +14,10 @@ s = doc.paragraphs[0]
 # s.paragraph_format.space_after = Inches(0.5)
 # print(dir(s))
 r = s.add_run("胶州市胶东街道办事处文件")
-r.font.name = "方正小标宋_GBK"
+r.font.name = '方正小标宋_GBK'
+r._element.rPr.rFonts.set(qn('w:eastAsia'), u'方正小标宋_GBK')
 r.font.size = Pt(72)
-r.font.bold = True
+# r.font.bold = True
 r.font.color.rgb = RGBColor(255,0,0)
 
 
